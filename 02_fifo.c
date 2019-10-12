b3be44d371dc804fd695c3fc3d38e072de2e72e6
positoy <positoy@gmail.com>
2019-10-13 08:11:50 +0900

    [IPC] 02 fifo - minor fix

--- 02_fifo.c
+++ 02_fifo.c
@@ -5,51 +5,77 @@
 #include <sys/stat.h>
 #include <string.h>
 
-const char FIFO_FILEPATH[] = "./fifofd"; 
+#define FIFO_FILEPATH   "./fifofd"
+#define handle_error(msg)       do{\
+                                    perror(msg);\
+                                    return -1;\
+                                } while(0)
+#define print_usage(filepath)   printf("usage: %s <send|recv>\n", filepath)
 
-#if defined(SERVER)
-int main()
+int fifo_send()
 {
     unlink(FIFO_FILEPATH);
-    if (mkfifo(FIFO_FILEPATH, 0644) == -1)
-        perror("failed to mkfifo");
+    if (mkfifo(FIFO_FILEPATH, 0666) == -1)
+        handle_error("failed to mkfifo");
 
     int fd = open(FIFO_FILEPATH, O_WRONLY);
+    if (fd == -1)
+        handle_error("failed to open");
 
     puts("client connected.");
 
     char wrdata = 'a';
-
     while (1) {
         sleep(1);
         if (write(fd, &wrdata, 1) == -1)
-            perror("failed to write");
-        else
-            printf("server write (%c)", wrdata);
+            handle_error("failed to write");
+
+        printf("sent (%c)\n", wrdata);
         wrdata = (wrdata+1) > 'z' ? 'a' : wrdata+1;
     }
 
     close(fd);
-    
+    return 0;
 }
-#elif defined(CLIENT)
-int main()
+
+int fifo_recv()
 {
     int fd = open(FIFO_FILEPATH, O_RDONLY);
+    if (fd == -1)
+        handle_error("failed to open");
 
     puts("connected to a server.");
 
     char rdbuf[10];
     while (1) {
-        sleep(1);
         memset(rdbuf, 0, sizeof(rdbuf));
         if (read(fd, rdbuf, sizeof(rdbuf)) == -1)
-            perror("failed to read");
-        else
-            printf("client read (%s)\n", rdbuf);
+            handle_error("failed to read");
+
+        printf("recv (%s)\n", rdbuf);
     }
 
     close(fd);
+    return 0;
 }
-#endif
 
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        print_usage(argv[0]);
+        return -1;
+    }
+
+    if (!strcmp(argv[1], "send")) {
+        if (fifo_send())
+            return -1;
+    } else if (!strcmp(argv[1], "recv")) {
+        if (fifo_recv())
+            return -1;
+    } else {
+        print_usage(argv[1]);
+        return -1;
+    }
+
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
-all:	pipe.out fifo_server.out fifo_client.out mq_sysv.out mq_posix.out
+all:	 pipe.out fifo.out mq_sysv.out mq_posix.out socket_stream.out
 
 pipe.out:	01_pipe.c
 	gcc -g -Wall 01_pipe.c -o pipe.out
 
-fifo_server.out:	02_fifo.c
-	gcc -g -Wall 02_fifo.c -o fifo_server.out -DSERVER
-
-fifo_client.out:	02_fifo.c
-	gcc -g -Wall 02_fifo.c -o fifo_client.out -DCLIENT
+fifo.out:	02_fifo.c
+	gcc -g -Wall 02_fifo.c -o fifo.out
 
 mq_sysv.out:	03_mq_sysv.c
 	gcc -g -Wall 03_mq_sysv.c -o mq_sysv.out
@@ -15,5 +12,8 @@ mq_sysv.out:	03_mq_sysv.c
 mq_posix.out:	04_mq_posix.c
 	gcc -g -Wall 04_mq_posix.c -o mq_posix.out -lrt
 
+socket_stream.out:	05_socket_stream.c
+	gcc -g -Wall 05_socket_stream.c -o socket_stream.out
+
 clean:
-	rm -rf *.out fifofd
+	rm -rf *.out fifofd sockstreamfd
